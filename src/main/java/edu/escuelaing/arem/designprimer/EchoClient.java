b213cb8207a059d11b5e4c11082df3c3d965eef5
Jonathan Prieto <jonnhi.prietou@gmail.com>
2018-09-07 16:57:06 -0500

    Finalizado

--- src/main/java/edu/escuelaing/arem/designprimer/EchoClient.java
+++ src/main/java/edu/escuelaing/arem/designprimer/EchoClient.java
@@ -15,17 +15,12 @@ import java.net.*;
 public class EchoClient {
 
     public static void main(String[] args) throws Exception {
-        URL google = new URL("https://immense-escarpment-48819.herokuapp.com/result?realNumber=1%2C2%2C3");
+        URL heroku = new URL("http://localhost:4567/result?realNumber=1%2C2%2C3");
         try (BufferedReader reader
-                = new BufferedReader(new InputStreamReader(google.openStream()))) {
+                = new BufferedReader(new InputStreamReader(heroku.openStream()))) {
             String inputLine = null;
-            String respuesta = null;
-            boolean flag = false;
             while ((inputLine = reader.readLine()) != null) {
-                String[] lineas = inputLine.split(";");
-                for (String s : lineas) {
-                    System.out.println(s);
-                }
+                System.out.println(inputLine);
             }
         } catch (IOException x) {
             System.err.println(x);
